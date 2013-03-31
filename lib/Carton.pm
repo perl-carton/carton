08f89302cc2edbf891f09674cab56ce5a1770f79
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-03-30 18:25:50 -0700

    v0.9.14

--- lib/Carton.pm
+++ lib/Carton.pm
@@ -3,7 +3,7 @@ package Carton;
 use strict;
 use warnings;
 use 5.008_001;
-use version; our $VERSION = version->declare("v0.9.13");
+use version; our $VERSION = version->declare("v0.9.14");
 
 use Cwd;
 use Config qw(%Config);
