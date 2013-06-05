5a350c90bafc1bf753ab91f3777230132ead084e
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-06-05 11:21:42 +0900

    Add Dependency#version method to find the version in the dists locked

--- lib/Carton/Dependency.pm
+++ lib/Carton/Dependency.pm
@@ -2,13 +2,18 @@ package Carton::Dependency;
 use strict;
 use Moo;
 
-has module  => (is => 'rw');
+has module => (is => 'rw');
 has requirement => (is => 'rw');
-has dist    => (is => 'rw', handles => [ qw(prereqs) ]);
+has dist => (is => 'rw', handles => [ qw(prereqs) ]);
 
 sub distname {
     my $self = shift;
     $self->dist->dist;
+}
+
+sub version {
+    my $self = shift;
+    $self->dist->provides->{$self->module}{version};
 }
 
 1;
