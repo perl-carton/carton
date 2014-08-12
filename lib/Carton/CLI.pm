f8299dc3ef2fbffcc723524d82f7b52f5080245e
ikasam_a <masaki.nakagawa@gmail.com>
2014-08-12 21:34:42 +0900

    skip installed modules (cpanm use -l instead of -L)

--- lib/Carton/CLI.pm
+++ lib/Carton/CLI.pm
@@ -177,11 +177,12 @@ sub cmd_install {
 
     $self->parse_options(
         \@args,
-        "p|path=s"    => \$install_path,
-        "cpanfile=s"  => \$cpanfile_path,
-        "without=s"   => sub { push @without, split /,/, $_[1] },
-        "deployment!" => \my $deployment,
-        "cached!"     => \my $cached,
+        "p|path=s"        => \$install_path,
+        "cpanfile=s"      => \$cpanfile_path,
+        "without=s"       => sub { push @without, split /,/, $_[1] },
+        "deployment!"     => \my $deployment,
+        "cached!"         => \my $cached,
+        "skip-installed!" => \my $skip_installed,
     );
 
     my $env = Carton::Environment->build($cpanfile_path, $install_path);
@@ -196,6 +197,7 @@ sub cmd_install {
         mirror  => $self->mirror,
         without => \@without,
         cpanfile => $env->cpanfile,
+        skip_installed => $skip_installed,
     );
 
     # TODO: --without with no .lock won't fetch the groups, resulting in insufficient requirements
