b83bc2a22315c35fcd124e1ca649b633738cc9ac
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2018-04-22 13:15:23 +0200

    document dist, url and mirror

--- lib/Carton.pm
+++ lib/Carton.pm
@@ -83,6 +83,24 @@ the modules you just installed.
   > git add cpanfile cpanfile.snapshot
   > git commit -m "Added Plack and Starman"
 
+=head2 Specifying a CPAN distribution
+
+You can pin a module resolution to a specific distribution using a
+combination of C<dist>, C<mirror> and C<url> options in C<cpanfile>.
+
+  # specific distribution on PAUSE
+  requires 'Plack', '0.9980',
+    dist => 'MIYAGAWA/Plack-0.9980.tar.gz';
+
+  # local mirror (darkpan)
+  requires 'Plack', '0.9981',
+    dist => 'MYCOMPANY/Plack-0.9981-p1.tar.gz',
+    mirror => 'https://pause.local/';
+
+  # URL
+  requires 'Plack', '1.1000',
+    url => 'https://pause.local/authors/id/M/MY/MYCOMPANY/Plack-1.1000.tar.gz';
+
 =head2 Deploying your application
 
 Once you've done installing all the dependencies, you can push your
